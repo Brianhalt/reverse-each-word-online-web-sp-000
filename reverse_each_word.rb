8c8015d3905d93883efaaac6517ea694323048fc
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 20:39:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,11 @@ require 'pry'
 
 def reverse_each_word(sentence1)
   original_array = sentence1.split(" ")
+  binding.pry
   new_array []
   original_array.each do|sentence1|
     new_array << sentence1.reversed
-    binding.pry
+    # binding.pry
   end
   # new_array.join(" ")
 end
