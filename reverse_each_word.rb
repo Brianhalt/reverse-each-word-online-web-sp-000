3ec92941757fe1b520f22a9c5c6760903c55a9f5
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 19:47:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,11 @@ require 'pry'
 original_array = ["Hello there, and how are you?"]
 
 def reverse_each_word(original_array)
-  original_array = string.split(" ")
+  original_array = original_array.split(" ")
   binding.pry
   new_array []
   original_array.each do|word|
-    new_array << string.reversed
+    new_array << original_array.reversed
   end
   new_array.join(" ")
 end
