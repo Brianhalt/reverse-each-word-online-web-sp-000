b95163c12556b3a5603cf0ee6866f244fcf5c12c
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 20:18:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require 'pry'
-# binding.pry
 
 
-def reverse_each_word(string)
-  original_array = string.split(" ")
+def reverse_each_word(sentence1)
+  original_array = sentence1.split(" ")
+  binding.pry
   new_array []
   original_array.each do|string|
     new_array << string.reversed
